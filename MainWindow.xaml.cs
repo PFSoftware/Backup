25bf790ab241744b42a2379d605b42c8eaf6e8ee
PFSoftware <PFSoftware@protonmail.com>
2020-06-15 23:16:11 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace PFSoftware.Backup
+{
+    /// <summary>Interaction logic for MainWindow.xaml</summary>
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
